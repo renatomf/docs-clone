a2c3abac4295ac8b2a0736431367a7c2d78b9063
renatomarf76 <renatomarf76@egmail.com>
2025-02-10 22:28:51 -0300

    authentication setup (clerk)

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://adjusted-impala-29.clerk.accounts.dev",
+      applicationID: "convex",
+    }
+  ]
+}
